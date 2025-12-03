11733547daf5549d852a7b41311aad1d6854a269
Skullman-G <skullmanslashgames@gmail.com>
2025-12-03 18:17:54 +0100

    remove server mentions in nix module

--- ldbgames.nix
+++ ldbgames.nix
@@ -4,39 +4,21 @@ let
 in
 {
   options.programs.ldbgames = {
-    client = {
-      enable = lib.mkEnableOption "Enable client";
-      serverIp = lib.mkOption {
-        type = lib.types.str;
-        default = "0.0.0.0";
-        description = "The Ip adress of the server";
-      };
-    };
-    server = {
-      enable = lib.mkEnableOption "Enable server";
+    enable = lib.mkEnableOption "Enable client";
+    serverIp = lib.mkOption {
+      type = lib.types.str;
+      default = "0.0.0.0";
+      description = "The Ip adress of the server";
     };
   };
 
-  config = {
-    services.nginx = lib.mkIf cfg.server.enable {
-      enable = true;
-      virtualHosts."ldbgames.com" = {
-        root = "/var/cache/ldbgames";
-        enableACME = false;
-        extraConfig = ''
-          gzip off;
-        '';
-      };
-    };
-
-    networking.firewall.allowedTCPPorts = lib.mkIf cfg.server.enable [ 80 ];
-
-    environment.systemPackages = lib.mkIf cfg.client.enable [
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [
       ldbgames-package
     ];
 
-    networking.extraHosts = lib.mkIf cfg.client.enable ''
-      ${cfg.client.serverIp} ldbgames.com
+    networking.extraHosts = ''
+      ${cfg.serverIp} ldbgames.com
     '';
   };
 }
